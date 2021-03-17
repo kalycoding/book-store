9b7c38f434873901a0b2fa1b39660f070e1d709a
kalycoding <muhdkhalifa1@yahoo.com>
2021-03-17 17:23:50 +0100

    migrate to sqlite

--- books/models.py
+++ books/models.py
@@ -1,8 +1,15 @@
 from django.db import models
 from django.contrib.auth import get_user_model
+from django.urls import reverse
+import uuid
 # Create your models here.
 
 class Book(models.Model):
+    id = models.UUIDField(
+        primary_key=True,
+        default=uuid.uuid4,
+        editable=False
+    )
     title = models.CharField(max_length=250)
     author = models.ForeignKey(get_user_model(), on_delete=models.CASCADE)
     price = models.DecimalField(max_digits=6, decimal_places=2)
@@ -10,4 +17,22 @@ class Book(models.Model):
 
     def __str__(self):
         return self.title
+    
+    def get_absolute_url(self):
+        return reverse('book_detail', kwargs={'pk': self.pk})
+
+
+class Review(models.Model):
+    book = models.ForeignKey(
+        Book,
+        on_delete=models.CASCADE
+    )
+    review = models.CharField(max_length=200)
+    author = models.ForeignKey(
+        get_user_model(),
+        on_delete=models.CASCADE
+    )
+
+    def __str__(self):
+        return self.review
     
--- bookstore_project/settings.py
+++ bookstore_project/settings.py
@@ -91,16 +91,10 @@ AUTH_USER_MODEL = 'users.User'
 
 DATABASES = {
     'default': {
-        
-        'ENGINE': 'django.db.backends.postgresql',
-        'NAME': 'books',
-        'USER': 'postgres',
-        'PASSWORD': 'c9mxQKJ*!T',
-        'HOST': 'localhost',
-        'PORT': 5432
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': BASE_DIR / 'db.sqlite3',
     }
 }
-
 
 # Password validation
 # https://docs.djangoproject.com/en/3.1/ref/settings/#auth-password-validators
